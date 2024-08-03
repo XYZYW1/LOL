bbfdeaed5f271cafa28877aff32db98c8dcf707f
XYZYW1 <159972291+XYZYW1@users.noreply.github.com>
2024-08-04 01:11:28 +0800

    Update and rename .gitignore to project stracture.js

---
+++ project stracture.js
@@ -0,0 +1,5 @@
+/index.html         // Main HTML file
+/styles.css         // CSS file for styling
+/scripts.js         // JavaScript file for interactivity
+/images/            // Folder for images
+/songs/             // Folder for audio files
